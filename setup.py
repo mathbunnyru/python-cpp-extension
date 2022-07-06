f1db6460bb47dd163bbf3c164e7a79ceb16ed1a7
Ayaz Salikhov <mathbunnyru@gmail.com>
2022-07-06 10:15:11 +0400

    Add pre-commit hooks

--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
-from setuptools import setup, Extension
 import sysconfig
 
 from Cython.Build import cythonize
+from setuptools import Extension, setup
 
+language = "c++"
+std = "c++17"
 
-language = 'c++'
-std = 'c++17'
-
-default_compile_args = sysconfig.get_config_var('CFLAGS').split()
+default_compile_args = sysconfig.get_config_var("CFLAGS").split()
 extra_compile_args = [f"-std={std}", "-Wall", "-Wextra", "-Werror", "-DNDEBUG", "-O3"]
 
-print(f'Default compile arguments: {default_compile_args}')
-print(f'Extra compile arguments: {extra_compile_args}')
+print(f"Default compile arguments: {default_compile_args}")
+print(f"Extra compile arguments: {extra_compile_args}")
 
 extension = Extension(
-    'cpp_python_extension',
-    sources=['sieve_python_wrapper.cpp', 'sieve.cpp'],
+    "cpp_python_extension",
+    sources=["sieve_python_wrapper.cpp", "sieve.cpp"],
     extra_compile_args=extra_compile_args,
-    language=language
+    language=language,
 )
 
 cython_extension = Extension(
-    'cython_sieve',
-    sources=['cython_sieve.pyx', 'sieve.cpp'],
+    "cython_sieve",
+    sources=["cython_sieve.pyx", "sieve.cpp"],
     extra_compile_args=extra_compile_args,
-    language='c++',
+    language="c++",
 )
 
 setup(
-    name='cpp_python_extension',
-    version='1.0',
-    description='This is Example module written in C++',
+    name="cpp_python_extension",
+    version="1.0",
+    description="This is Example module written in C++",
     ext_modules=cythonize([extension, cython_extension]),
 )
--- test_benchmark.py
+++ test_benchmark.py
@@ -1,11 +1,12 @@
-import pytest
 import cpp_python_extension
 import cython_sieve
+import pytest
 
 
 @pytest.mark.parametrize("input", [1000, 100000, 1000000])
 def test_cpp(benchmark, input):
     benchmark(cpp_python_extension.sieve_of_eratosthenes, input)
+
 
 @pytest.mark.parametrize("input", [1000, 100000, 1000000])
 def test_cyth(benchmark, input):
