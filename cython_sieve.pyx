9bd307674f8237ebd0b0a4f38740fb121d392841
Fedor Alekseev <feodor.alexeev@gmail.com>
2022-07-06 08:10:34 +0200

    Add an example of the same wrapper using cython (#2)
    * Add an example of the same wrapper using cython
    
    * Update .gitignore
    
    * Update cython_sieve.pyx
    
    * Update sieve.pxd
    
    Co-authored-by: Ayaz Salikhov <mathbunnyru@users.noreply.github.com>

---
+++ cython_sieve.pyx
@@ -0,0 +1,8 @@
+cimport sieve
+
+
+def sieve_of_eratosthenes(size_t n):
+    '''
+    Return list of prime numbers until given number. Implemented in Cython.
+    '''
+    return sieve.SieveOfEratosthenes(n)
